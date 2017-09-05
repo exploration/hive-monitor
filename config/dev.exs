46891fe3466cc1a64744c95dd8abb789a2ba5489
Donald Merand <dmerand@explo.org>
2017-09-04 21:49:44 -0400

    show metadata and time in dev logs

--- config/dev.exs
+++ config/dev.exs
@@ -5,4 +5,4 @@ use Mix.Config
 #
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+#config :logger, :console, format: "[$level] $message\n"
