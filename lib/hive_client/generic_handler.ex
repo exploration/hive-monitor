92f4fbbed78bb78858454cbd96924d54212af9ed
Donald Merand <dmerand@explo.org>
2017-09-08 20:59:12 -0400

    remove osascript calls, do nothing extra in these handlers (for now)

--- lib/hive_client/generic_handler.ex
+++ lib/hive_client/generic_handler.ex
@@ -1,6 +1,7 @@
 defmodule HiveClient.GenericHandler do
   def handle_atom(atom) when is_map(atom) do
-    System.cmd "/usr/bin/osascript", ["-e", "display dialog \"HIVE data received!\nApplication: #{atom["application"]}\nContext: #{atom["context"]}\nProcess: #{atom["process"]}\""]
+    #System.cmd "/usr/bin/osascript", ["-e", "display dialog \"HIVE data received!\nApplication: #{atom["application"]}\nContext: #{atom["context"]}\nProcess: #{atom["process"]}\""]
+    IO.puts("Generic Handler got the atom")
   end
 end
 
