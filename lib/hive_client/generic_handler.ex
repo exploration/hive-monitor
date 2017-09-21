573401888b241674cdb6768b80c56482fb2e501c
Donald Merand <dmerand@explo.org>
2017-09-21 12:49:34 -0400

    GenericHandler now outputs the triplet when it receives a thing

--- lib/hive_client/generic_handler.ex
+++ lib/hive_client/generic_handler.ex
@@ -1,7 +1,7 @@
 defmodule HiveClient.GenericHandler do
   def handle_atom(atom) when is_map(atom) do
     #System.cmd "/usr/bin/osascript", ["-e", "display dialog \"HIVE data received!\nApplication: #{atom["application"]}\nContext: #{atom["context"]}\nProcess: #{atom["process"]}\""]
-    IO.puts("Generic Handler got the atom")
+    IO.puts("Generic Handler got the atom: (#{atom["application"]},#{atom["context"]},#{atom["process"]})")
   end
 end
 
