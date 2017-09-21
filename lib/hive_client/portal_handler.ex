9a21d01eb3c5afd70cf4a932e90b3efc47473706
Donald Merand <dmerand@explo.org>
2017-09-21 12:49:21 -0400

    update PortalHandler to actually send the data to a Portal endpoint

--- lib/hive_client/portal_handler.ex
+++ lib/hive_client/portal_handler.ex
@@ -1,6 +1,15 @@
 defmodule HiveClient.PortalHandler do
+  @portal_token 'dd674122358db45f7a1f76e11328ed20'
+  @portal_url 'https://portal.explo.org/hive/incoming_atoms'
+  #@portal_url 'http://localhost:3000/hive/incoming_atoms'
+
   def handle_atom(atom) when is_map(atom) do
-    #System.cmd "/usr/bin/osascript", ["-e", "display dialog \"Portal data received!\nApplication: #{atom["application"]}\nContext: #{atom["context"]}\nProcess: #{atom["process"]}\""]
-    IO.puts("Portal Handler got the atom")
+    atom_json = "#{atom |> Poison.encode! |> URI.encode}"
+    body = "atom=#{atom_json}&portal_token=#{@portal_token}"
+    headers = [
+        "User-Agent": "HIVE Client",
+        "Content-Type": "application/x-www-form-urlencoded"
+    ]
+    HTTPotion.post @portal_url, [body: body, headers: headers]
   end
 end
