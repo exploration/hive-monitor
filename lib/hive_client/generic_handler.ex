f31b85b7854e1dbc3ef2187353728106116583b4
Donald Merand <dmerand@explo.org>
2017-09-22 15:44:30 -0400

    Send HipChat notice for unknown atoms. Resolves https://3.basecamp.com/3519819/buckets/1464660/todos/672228815

--- lib/hive_client/generic_handler.ex
+++ lib/hive_client/generic_handler.ex
@@ -1,7 +1,27 @@
 defmodule HiveClient.GenericHandler do
+  @hipchat_api_url "https://api.hipchat.com/v2"
+  @hipchat_room "143945"
+  @hipchat_token "AwS0F1sbBYlPPSJOWwITASwr7yslZBi9sVxJI10S"
+  @recipients ["Donald"]
+
+  # The generic case is that we
   def handle_atom(atom) when is_map(atom) do
-    #System.cmd "/usr/bin/osascript", ["-e", "display dialog \"HIVE data received!\nApplication: #{atom["application"]}\nContext: #{atom["context"]}\nProcess: #{atom["process"]}\""]
-    IO.puts("Generic Handler got the atom: (#{atom["application"]},#{atom["context"]},#{atom["process"]})")
+    message = "Generic Handler got the atom: (#{atom["application"]}, #{atom["context"]}, #{atom["process"]})"
+    IO.puts message
+    body = "from=HIVE Monitor&format=text&notify=true&message=#{format_recipients()} #{message}"
+      |> URI.encode
+    headers = [
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Bearer #{@hipchat_token}"
+    ]
+    HTTPotion.post "#{@hipchat_api_url}/room/#{@hipchat_room}/notification", 
+        [body: body, headers: headers]
+  end
+
+  defp format_recipients do
+    @recipients
+      |> Enum.map(&("@" <> &1))
+      |> Enum.join(" ")
   end
 end
 
