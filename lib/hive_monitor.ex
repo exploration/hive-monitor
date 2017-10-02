acff48aa29244ad5521ddb53de11e890810b9b54
Donald Merand <dmerand@explo.org>
2017-10-01 20:51:05 -0400

    update the application to use the global supervisor instead of declaring everything right in the application file

--- lib/hive_monitor.ex
+++ lib/hive_monitor.ex
@@ -4,29 +4,7 @@ defmodule HiveMonitor do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      # Start the endpoint when the application starts
-      worker(HiveMonitor.SocketClient, []),
-      worker(HiveMonitor.Router, []),
-
-      # Also start any "cron" tasks we want to run concurrent with Atom synching
-      cron_child("/bin/echo", ["ping"], :timer.seconds(60), true, "test1"),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: HiveMonitor.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
-
-  defp cron_child(cmd, args, rate, run_on_start, id) do
-    Supervisor.child_spec( { HiveMonitor.CronServer, 
-        %HiveMonitor.CronServer.State{ 
-            cmd: cmd, args: args, rate: rate, run_on_start: run_on_start} 
-        }, 
-        id: id
-    )
+    IO.puts "Starting HIVE Monitoring"
+    HiveMonitor.Supervisor.start_link()    
   end
 end
