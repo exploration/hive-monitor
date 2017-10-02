d2349ced74cdcfd7eb62c7543c9c0918d42288b3
Donald Merand <dmerand@explo.org>
2017-10-01 20:49:30 -0400

    Add a supervisor for the socket client

---
+++ lib/hive_monitor/socket/socket_supervisor.ex
@@ -0,0 +1,13 @@
+defmodule HiveMonitor.SocketSupervisor do
+  use Supervisor
+
+  def start_link(_args) do
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    Supervisor.init([
+      HiveMonitor.SocketClient
+    ], strategy: :one_for_one)
+  end
+end
