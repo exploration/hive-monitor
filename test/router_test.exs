b67ce0e04940dfaa4324f3e38d05c2c853a0f269
Donald Merand <dmerand@explo.org>
2017-10-02 21:27:22 -0400

    clean up test syntax a bit

--- test/router_test.exs
+++ test/router_test.exs
@@ -1,53 +1,60 @@
 defmodule RouterTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
+  alias HiveMonitor.Router
 
   setup do
-    triplet = {"test_application","test_process","test_context"}
+    atom = %{
+      "application" => "test_application",
+      "context" => "test_context",
+      "process" => "test_process",
+      "data" => ~s({"hello":"world"})
+    }
+    triplet = {atom["application"],atom["context"],atom["process"]}
     handler = HiveMonitor.GenericHandler
-    handler2 = HiveMonitor.FakeHandler
+    test_handler = HiveMonitor.TestHandler
+
     {
       :ok,
       triplet: triplet,
       handler: handler,
-      handler2: handler2
+      test_handler: test_handler,
+      atom: atom
     }
     
   end
 
-  describe "Test adding + removing handlers" do
+  describe "Test adding + removing handlers: " do
     test "Known triplets is returned by add_handler and remove_handler", %{triplet: triplet, handler: handler} do
-      known_triplets = HiveMonitor.Router.known_triplets()
-      refute Enum.empty? known_triplets
 
-      added_triplets = HiveMonitor.Router.add_handler(triplet, handler)
+      added_triplets = Router.add_handler(triplet, handler)
+      known_triplets = Router.known_triplets()
+      refute Enum.empty? known_triplets
       assert added_triplets == known_triplets
 
-      removed_triplets = HiveMonitor.Router.remove_handler(triplet, handler)
-      known_triplets = HiveMonitor.Router.known_triplets()
+      removed_triplets = Router.remove_handler(triplet, handler)
+      known_triplets = Router.known_triplets()
       assert removed_triplets == known_triplets
     end
 
     test "Adding a handler modifies known triplets with a new entry", %{triplet: triplet, handler: handler} do
-      known_triplets = HiveMonitor.Router.known_triplets()
-      added_triplets = HiveMonitor.Router.add_handler(triplet, handler)
+      added_triplets = Router.add_handler(triplet, handler)
       assert {:ok, [HiveMonitor.GenericHandler]} == Map.fetch(added_triplets, triplet)
-      refute known_triplets == added_triplets
     end
     
     test "Removing a unique handler removes the triplet from known_triplets", %{triplet: triplet, handler: handler} do
-      HiveMonitor.Router.add_handler(triplet, handler)
-      known_triplets = HiveMonitor.Router.remove_handler(triplet, handler)
+      Router.add_handler(triplet, handler)
+      known_triplets = Router.remove_handler(triplet, handler)
 
       assert :error == Map.fetch(known_triplets, triplet)
     end
     
-    test "Removing a non-unique handler keeps the triplet entry", %{triplet: triplet, handler: handler, handler2: handler2} do
-      HiveMonitor.Router.add_handler(triplet, handler)
-      HiveMonitor.Router.add_handler(triplet, handler2)
-      known_triplets = HiveMonitor.Router.remove_handler(triplet, handler2)
+    test "Removing a non-unique handler keeps the triplet entry", %{triplet: triplet, handler: handler, test_handler: test_handler} do
+      Router.add_handler(triplet, handler)
+      Router.add_handler(triplet, test_handler)
+      known_triplets = Router.remove_handler(triplet, test_handler)
 
       assert {:ok, [handler]} == Map.fetch(known_triplets, triplet)
     end
   end
-  
+
 end
