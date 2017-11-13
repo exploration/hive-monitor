c9fcef87e54a02d18117493d0ba7b4ef2a293477
Donald Merand <dmerand@explo.org>
2017-11-13 17:36:53 -0500

    update with admin notify

--- lib/hive_monitor/handlers/generic_handler.ex
+++ lib/hive_monitor/handlers/generic_handler.ex
@@ -15,17 +15,26 @@ defmodule HiveMonitor.GenericHandler do
   @impl true
   def handle_atom(%HiveAtom{} = atom) do
     message = "Generic Handler got the atom: (#{atom.application}" <>
-        ", #{atom.context}, #{atom.process})"
+        ", #{atom.context}, #{atom.process}) " <>
+        "data: #{inspect atom.data}"
     Logger.info(fn -> message end)
 
     recipients = ["Donald"]
     {:ok, %{status_code: status_code}} = 
-      ExploComm.HipChat.send_notification(message, recipients)
+      ExploComm.HipChat.send_notification(notify() <> message, recipients)
 
     case(Enum.member?(200..299, status_code)) do
       true -> {:ok, :success}
       false -> :error
     end
+  end
+
+  defp notify do 
+    admins = ["Donald"]
+    admins
+    |> Enum.map(fn x -> "@#{x}" end)
+    |> Enum.join(" ")
+    |> Kernel.<>(" ")
   end
 
 end
