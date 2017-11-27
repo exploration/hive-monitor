7bd28d7f250c3f1df795a35bf76e7f17766b11e3
Donald Merand <dmerand@explo.org>
2017-11-26 20:45:36 -0500

    remove hipchat logging from generic handler

--- lib/hive_monitor/handlers/generic_handler.ex
+++ lib/hive_monitor/handlers/generic_handler.ex
@@ -1,14 +1,12 @@
 defmodule HiveMonitor.GenericHandler do
 
   @moduledoc """
-  The generic case is that we send the atom to HipChat, and notify IT.
+  The generic case is merely to log the atom
   """
 
   @behaviour HiveMonitor.Handler
 
   require Logger
-
-  alias ExploComm.HipChat
 
   @doc false
   @impl true
@@ -22,27 +20,7 @@ defmodule HiveMonitor.GenericHandler do
       "data: #{inspect atom.data}"
     Logger.info(fn -> message end)
 
-    {:ok, %{status_code: status_code}} =
-      HipChat.send_notification(
-        notify() <> message, mentions: recipients()
-      )
-
-    case(Enum.member?(200..299, status_code)) do
-      true -> {:ok, :success}
-      _ -> :error
-    end
-  end
-
-  defp notify() do
-    admins = ["Donald"]
-    admins
-    |> Enum.map(fn x -> "@#{x}" end)
-    |> Enum.join(" ")
-    |> Kernel.<>(" ")
-  end
-
-  defp recipients() do
-    Application.get_env(:hive_monitor, :default_chat_recipients) || ["Donald"]
+    {:ok, :success}
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HiveMonitor.Mixfile do
   def project do
     [
       app: :hive_monitor,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: Mix.compilers,
