2423732bef88c550fe9c49a1e6532dfc2dd81aea
Donald Merand <dmerand@explo.org>
2018-02-09 10:30:14 -0500

    version bump for ex_doc

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule HiveMonitor.Mixfile do
       app: :hive_monitor,
       name: "HIVE Monitor",
       source_url: "https://bitbucket.org/explo/hive-monitor",
-      version: "0.5.7",
+      version: "0.5.8",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
