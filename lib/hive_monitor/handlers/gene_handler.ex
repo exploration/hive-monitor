f8d43edb34a6916c7e40b25de3a4f5eec86f06dd
Donald Merand <dmerand@explo.org>
2018-11-01 21:54:29 -0400

    add GENE handler

---
+++ lib/hive_monitor/handlers/gene_handler.ex
@@ -0,0 +1,35 @@
+defmodule HiveMonitor.GeneHandler do
+  @moduledoc """
+  Forward an atom to the Gene app's incoming_atom endpoint.
+  """
+
+  alias HiveMonitor.Handler
+  @behaviour Handler
+
+  @token "3a9555043bea1ff8892914b8deceb0fd523243d0b"
+  @url "https://explo-gene.herokuapps.com/hive/incoming_atoms"
+
+  @doc false
+  @impl true
+  def application_name, do: "gene_production"
+
+  @doc false
+  @impl true
+  def handle_atom(%HiveAtom{} = atom) do
+    atom_encoded = Handler.atom_to_uri_form(atom)
+
+    body = "atom=#{atom_encoded}&token=#{@token}"
+
+    headers = [
+      {"User-Agent", "HIVE Monitor"},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+
+    %{status_code: status_code} = HTTPoison.post!(@url, body, headers)
+
+    case Enum.member?(200..299, status_code) do
+      true -> {:ok, :success}
+      false -> :error
+    end
+  end
+end
