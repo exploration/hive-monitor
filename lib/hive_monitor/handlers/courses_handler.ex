8024bdaf8d1c7e23dc91bf63cc88c03bbc72d9f4
Donald Merand <dmerand@explo.org>
2019-03-31 20:51:41 -0400

    add courses handler

---
+++ lib/hive_monitor/handlers/courses_handler.ex
@@ -0,0 +1,35 @@
+defmodule HiveMonitor.CoursesHandler do
+  @moduledoc """
+  Forward an atom to the Courses app's incoming_atom endpoint.
+  """
+
+  alias HiveMonitor.Handler
+  @behaviour Handler
+
+  @token "b4585f440874a30fa24b76f8dad3b362d"
+  @url "https://courses.explo.org/hive/incoming_atoms"
+
+  @doc false
+  @impl true
+  def application_name, do: "courses_production"
+
+  @doc false
+  @impl true
+  def handle_atom(%HiveAtom{} = atom) do
+    atom_encoded = Handler.atom_to_uri_form(atom)
+
+    body = "atom=#{atom_encoded}&token=#{@token}"
+
+    headers = [
+      {"User-Agent", "HIVE Monitor"},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+
+    %{status_code: status_code} = HTTPoison.post!(@url, body, headers)
+
+    case Enum.member?(200..299, status_code) do
+      true -> {:ok, :success}
+      false -> :error
+    end
+  end
+end
