abfd1352ee1ebf3451a6cafe06483db6e6e94010
Donald Merand <dmerand@explo.org>
2021-02-27 19:32:31 -0500

    add module documentation

--- lib/hive_monitor.ex
+++ lib/hive_monitor.ex
@@ -1,5 +1,42 @@
 defmodule HiveMonitor do
-  @moduledoc false
+  @moduledoc """
+  This app subscribes to real-time Atom updates from
+  [HIVE](https://bitbucket.org/explo/hive-2), and can run scripts on the local
+  machine in response to them. You would use it to avoid having to long-poll
+  HIVE's REST API and instead get results in closer to real-time.
+
+  The application can also run local scripts or Elixir functions periodically, in
+  a similar way to cron, which makes it easier to keep all synchronization in one
+  area. See `HiveMonitor.CronServer` for details of how to set that up.
+
+  # Setup
+
+  You'll want to update `config/config.exs` in your app/deployment with some API
+  token variables:
+
+      # Monitor-specific setup
+      config :hive_monitor,
+        hive_socket_token: "your token",
+        default_chat_url: "https://chat.googleapis.com/somethin"
+        
+      # We also need to configure HIVE service, natch.
+      config :hive_service,
+        hive_api_token: "your token"
+
+  There are a few other possible/semi-optional configuration variables in this
+  system:
+
+      config :hive_monitor,
+        application_name: "can_be_customized",
+        router_config: "output of HiveMonitor.Router.get_config()"
+        crons: "output of HiveMonitor.CronServer.get_config()",
+        # The amount of time within which your initial batch of Crons will get
+        # randomly started:
+        cron_init_spread: :timer.minutes(3)
+
+  Check the related modules for more details about how to preconfigure/save
+  triplet/module maps and "Cron" jobs.
+  """
 
   use Application
 
