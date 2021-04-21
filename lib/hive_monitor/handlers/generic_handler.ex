393e0aaa63c476bae95859e8a3c65f1d7c6c1103
Donald Merand <dmerand@explo.org>
2021-04-21 19:56:40 -0400

    add individual atom ignoring

--- lib/hive_monitor/handlers/generic_handler.ex
+++ lib/hive_monitor/handlers/generic_handler.ex
@@ -6,6 +6,15 @@ defmodule HiveMonitor.Handlers.GenericHandler do
 
       config :hive_monitor,
         log_unrecognized_atoms: false
+
+  You can stop generic logging for an individual triplet (atom
+  identifier) by adding this to your config:
+
+      config :hive_monitor,
+        ignore_these_triplets: [
+          {"some", "app", "triplet"},
+          ...
+        ]
   """
 
   @behaviour HiveMonitor.Handler
@@ -19,7 +28,7 @@ defmodule HiveMonitor.Handlers.GenericHandler do
   @doc false
   @impl true
   def handle_atom(%HiveAtom{} = atom) do
-    if Application.get_env(:hive_monitor, :log_unrecognized_atoms, false) do
+    if we_are_logging_this(atom) do
       message =
         "Generic Handler got the atom: (#{atom.application}" <>
           ", #{atom.context}, #{atom.process}) data: #{inspect(atom.data)}"
@@ -28,5 +37,17 @@ defmodule HiveMonitor.Handlers.GenericHandler do
     end
 
     {:ok, :success}
+  end
+
+  defp log_unrecognized_atoms do
+    Application.get_env(:hive_monitor, :log_unrecognized_atoms, false)
+  end
+
+  defp triplets_of_ignoreville do
+    Application.get_env(:hive_monitor, :ignore_these_triplets, [])
+  end
+
+  defp we_are_logging_this(atom) do
+    log_unrecognized_atoms() and HiveAtom.triplet(atom) not in triplets_of_ignoreville() 
   end
 end
