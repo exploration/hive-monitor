1e01d99c5b4e49958f547c431b9dfd4699e8ca6b
Donald Merand <dmerand@explo.org>
2021-06-21 14:47:00 -0400

    refactor: hard LDAP overwrite when synching groups

--- bin/sync_ldap.sh
+++ bin/sync_ldap.sh
@@ -14,26 +14,18 @@ fi
 EXPLO="gringotts.explo.org"
 EXPLO_LDAP="/LDAPv3/${EXPLO}"
 GOOGLE_LDAP="/LDAPv3/ldap.google.com"
-SYNC_GROUPS="portico_director,portico_curriculum,portico_rd,portico_banker,portico_shopper,portico_staff,portico_triage"
+#SYNC_GROUPS="fm_facultyfeedbackadmin,hr_admin,hr_user,print_lists_admin,portico_director,portico_curriculum,portico_rd,portico_banker,portico_shopper,portico_staff,portico_triage"
 WORKDIR="/tmp"
 
 # export google users, but don't sync/overwrite password
 dsexport "${WORKDIR}/users.all" --N "${GOOGLE_LDAP}" dsRecTypeStandard:Users -e dsAttrTypeStandard:Password
 
-# omit suspended folks
 cat "${WORKDIR}/users.all" | awk '!/:true/{print $0}' > "${WORKDIR}/users"
 wc -l "${WORKDIR}/users"
 cat  "${WORKDIR}/users.all" | awk '/:true/{print $0}' > "${WORKDIR}/disabled_users"
 wc -l "${WORKDIR}/disabled_users"
 rm "${WORKDIR}/users.all"
 
-echo
-
-# export google groups
-dsexport "${WORKDIR}/groups" "${GOOGLE_LDAP}" dsRecTypeStandard:Groups -r "${SYNC_GROUPS}"
-wc -l "${WORKDIR}/groups"
-
-# import google groups
 echo "\nSynchronizing users..."
 dsimport "${WORKDIR}/users" "/LDAPv3/127.0.0.1" M --remotehost "${EXPLO}" --remoteusername "${USER}" --remotepassword "${PASS}" --username "${USER}" --password "${PASS}" --outputfile "${WORKDIR}/users.log"
 
@@ -42,5 +34,13 @@ cat "${WORKDIR}/disabled_users" |
 awk 'BEGIN{FS=":"}{print $5}' | 
 xargs -I xXx dscl -u "${USER}" -P "${PASS}" "${EXPLO_LDAP}" -delete /Users/xXx > /dev/null 2> /dev/null
 
+echo
+
+# export google groups
+#dsexport "${WORKDIR}/groups" "${GOOGLE_LDAP}" dsRecTypeStandard:Groups -r "${SYNC_GROUPS}"
+dsexport "${WORKDIR}/groups" "${GOOGLE_LDAP}" dsRecTypeStandard:Groups 
+wc -l "${WORKDIR}/groups"
+
+# import google groups
 echo "Synchronizing groups..."
-dsimport "${WORKDIR}/groups" "/LDAPv3/127.0.0.1" M --remotehost "${EXPLO}" --remoteusername "${USER}" --remotepassword "${PASS}" --username "${USER}" --password "${PASS}" --outputfile "${WORKDIR}/groups.log"
+dsimport "${WORKDIR}/groups" "/LDAPv3/127.0.0.1" O --remotehost "${EXPLO}" --remoteusername "${USER}" --remotepassword "${PASS}" --username "${USER}" --password "${PASS}" --outputfile "${WORKDIR}/groups.log"
